cc6ffead3c35cb54e98a8cf85a0a025f33b189f8
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-02-21 18:40:32 -0300

    Instanciando o RecyclerView e Adapter

--- app/src/main/java/guilhermekunz/com/br/cineflix/ui/MainActivity.kt
+++ app/src/main/java/guilhermekunz/com/br/cineflix/ui/MainActivity.kt
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import guilhermekunz.com.br.cineflix.R
 import guilhermekunz.com.br.cineflix.api.MoviesRepository
 import guilhermekunz.com.br.cineflix.data.Movie
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        popularMoviesAdapter = MoviesAdapter(listOf())
+        popularMovies.adapter = popularMoviesAdapter
 
         MoviesRepository.getPopularMovies(
             onSuccess = ::onPopularMoviesFetched,
