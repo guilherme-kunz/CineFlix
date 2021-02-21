949297d59d60d15b945da7e0b771a53c6d36cc67
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-02-21 16:17:51 -0300

    Implementação de novo metodo getPopularMovies

--- app/src/main/java/guilhermekunz/com/br/cineflix/api/MoviesRepository.kt
+++ app/src/main/java/guilhermekunz/com/br/cineflix/api/MoviesRepository.kt
@@ -1,5 +1,10 @@
 package guilhermekunz.com.br.cineflix.api
 
+import android.util.Log
+import guilhermekunz.com.br.cineflix.data.GetMoviesResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -14,6 +19,30 @@ object MoviesRepository {
             .build()
 
         api = retrofit.create(Api::class.java)
+    }
+
+    fun getPopularMovies(page: Int = 1) {
+        api.getPopularMovies(page = page)
+            .enqueue(object : Callback<GetMoviesResponse> {
+                override fun onResponse(
+                    call: Call<GetMoviesResponse>,
+                    response: Response<GetMoviesResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body()
+
+                        if (responseBody != null) {
+                            Log.d("Repository", "Movies: ${responseBody.movies}")
+                        } else {
+                            Log.d("Repository", "Failed to get response")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<GetMoviesResponse>, t: Throwable) {
+                    Log.e("Repository", "onFailure", t)
+                }
+            })
     }
 
 }
