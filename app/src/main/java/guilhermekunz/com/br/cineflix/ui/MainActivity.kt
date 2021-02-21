520e551f14ca795d40b5c1a40f4b0e038e3c02c4
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-02-21 16:25:44 -0300

    Implementando o função getPopularMovies

--- app/src/main/java/guilhermekunz/com/br/cineflix/ui/MainActivity.kt
+++ app/src/main/java/guilhermekunz/com/br/cineflix/ui/MainActivity.kt
@@ -2,14 +2,28 @@ package guilhermekunz.com.br.cineflix.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import guilhermekunz.com.br.cineflix.R
 import guilhermekunz.com.br.cineflix.api.MoviesRepository
+import guilhermekunz.com.br.cineflix.data.Movie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MoviesRepository.getPopularMovies()
+        MoviesRepository.getPopularMovies(
+            onSuccess = ::onPopularMoviesFetched,
+            onError = ::onError
+        )
+    }
+
+    private fun onPopularMoviesFetched(movies: List<Movie>) {
+        Log.d("MainActivity", "Movies: $movies")
+    }
+
+    private fun onError() {
+        Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
     }
 }
