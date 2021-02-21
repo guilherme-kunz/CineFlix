5e41d8747ed4c2081772e41b5c6290659b51b83e
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-02-21 16:10:45 -0300

    Criando uma nova classe de dados GetMoviesResponse.kt

---
+++ app/src/main/java/guilhermekunz/com/br/cineflix/data/GetMoviesResponse.kt
@@ -0,0 +1,9 @@
+package guilhermekunz.com.br.cineflix.data
+
+import com.google.gson.annotations.SerializedName
+
+data class GetMoviesResponse(
+    @SerializedName("page") val page: Int,
+    @SerializedName("results") val movies: List<Movie>,
+    @SerializedName("total_pages") val pages: Int
+)
