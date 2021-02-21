f54f5177c3e5a7d096ba7682698f1782d2c6bef8
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-02-21 16:31:09 -0300

    Implementando a classe MoviesAdapter

---
+++ app/src/main/java/guilhermekunz/com/br/cineflix/ui/MoviesAdapter.kt
@@ -0,0 +1,46 @@
+package guilhermekunz.com.br.cineflix.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import guilhermekunz.com.br.cineflix.R
+import guilhermekunz.com.br.cineflix.data.Movie
+
+class MoviesAdapter(
+    private var movies: List<Movie>
+) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    fun updateMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val poster: ImageView = itemView.findViewById(R.id.item_movie_poster)
+
+        fun bind(movie: Movie) {
+            Glide.with(itemView)
+                .load("https://image.tmdb.org/t/p/w342${movie.posterPath}")
+                .transform(CenterCrop())
+                .into(poster)
+        }
+    }
+}
